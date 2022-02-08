72c0559296bd1d5b1b24d84b0dbac8b4157c50d8
Krivilyov <gramprogramming@gmail.com>
2022-02-08 20:31:06 +0500

    fix config -image path

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 module.exports = {
 	reactStrictMode: true,
 	images: {
-		domains: ["localhost", "62.113.99.29:9000"],
-	},
-	webpackDevMiddleware: (config) => {
-		config.watchOptions = {
-			poll: 1000,
-			aggregateTimeout: 300,
-		};
-		return config;
+		domains: ["localhost", "api.my-texter.com"],
 	},
 };
