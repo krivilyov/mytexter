1b0582808a0742996c63f42e37f4b18db6d9554c
Vanya Krivilyov <ivan.k@miro.com>
2022-03-10 22:40:04 +0400

    tern off ESLinter for production

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextTranslate = require("next-translate");
 
 module.exports = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: !!process.env.ESLINTER,
+  },
   images: {
     domains: ["localhost", "api.my-texter.com"],
   },
